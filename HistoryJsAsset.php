08b482d89b77accc8ef8b1689dbea903671402e6
Maris Graudins <maris@mg-interactive.lv>
2016-06-09 13:59:20 +0300

    Checking history.js package dependency.

--- HistoryJsAsset.php
+++ HistoryJsAsset.php
@@ -2,6 +2,7 @@
 
 namespace mgcode\assets;
 
+use yii\base\InvalidConfigException;
 use yii\web\AssetBundle;
 
 class HistoryJsAsset extends AssetBundle
@@ -10,4 +11,12 @@ class HistoryJsAsset extends AssetBundle
     public $js = [
         'native.history.js',
     ];
+
+    public function init()
+    {
+        parent::init();
+        if(!file_exists(\Yii::getAlias('@bower/history.js/scripts/bundled/html5/native.history.js'))) {
+            throw new InvalidConfigException('You must include `bower-asset/history.js` package in your composer.json configruation file.');
+        }
+    }
 }
