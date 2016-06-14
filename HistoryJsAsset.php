cb5d5b6eacddc09890356caef7a4ae54766a8d66
Maris Graudins <maris@mg-interactive.lv>
2016-06-14 20:07:58 +0300

    Copyrights

--- HistoryJsAsset.php
+++ HistoryJsAsset.php
@@ -5,6 +5,10 @@ namespace mgcode\assets;
 use yii\base\InvalidConfigException;
 use yii\web\AssetBundle;
 
+/**
+ * @link https://github.com/mg-code/yii2-assets
+ * @author Maris Graudins <maris@mg-interactive.lv>
+ */
 class HistoryJsAsset extends AssetBundle
 {
     public $sourcePath = '@bower/history.js/scripts/bundled/html5';
